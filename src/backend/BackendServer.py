653e7a5464def92e262dd03878504030923cd2ea
H. Kerem Cevahir <hkerem@gmail.com>
2010-06-18 17:47:35 +0300

    Tiny refinements.

--- src/backend/BackendServer.py
+++ src/backend/BackendServer.py
@@ -39,74 +39,18 @@ from pdfminer.layout import LAParams
 
 import StringIO 
 
-import uno, unohelper
+from seqstream import *
+
+import uno
+
 from com.sun.star.connection import NoConnectException
 from com.sun.star.uno  import RuntimeException
 from com.sun.star.lang import IllegalArgumentException
 #
 from com.sun.star.beans import PropertyValue
-from com.sun.star.io import XSeekable, XInputStream, XOutputStream
 from com.sun.star.beans import PropertyValue
 
 import iban
-
-class SequenceOutputStream( unohelper.Base, XOutputStream ):
-      def __init__( self ):
-          self.s = uno.ByteSequence("")
-          self.closed = 0
-
-      def closeOutput(self):
-          self.closed = 1
-
-      def writeBytes( self, seq ):
-          self.s = self.s + seq
-
-      def flush( self ):
-          pass
-
-      def getSequence( self ):
-          return self.s
-
-class SequenceInputStream( XSeekable, XInputStream, unohelper.Base ):
-      def __init__( self, seq ):
-          self.s = seq
-          self.nIndex = 0
-          self.closed = 0
-
-      def closeInput( self):
-          self.closed = 1
-          self.s = None
-
-      def skipBytes( self, nByteCount ):
-          if( nByteCount + self.nIndex > len(self.s) ):
-              nByteCount = len(self.s) - self.nIndex
-          self.nIndex += nByteCount
-
-      def readBytes( self, retSeq, nByteCount ):
-          nRet = 0
-          if( self.nIndex + nByteCount > len(self.s) ):
-              nRet = len(self.s) - self.nIndex
-          else:
-              nRet = nByteCount
-          retSeq = uno.ByteSequence(self.s.value[self.nIndex : self.nIndex + nRet ])
-          self.nIndex = self.nIndex + nRet
-          return nRet, retSeq
-
-      def readSomeBytes( self, retSeq , nByteCount ):
-          #as we never block !
-          return readBytes( retSeq, nByteCount )
-
-      def available( self ):
-          return len( self.s ) - self.nIndex
-
-      def getPosition( self ):
-          return self.nIndex
-
-      def getLength( self ):
-          return len( self.s )
-
-      def seek( self, pos ):
-          self.nIndex = pos
 
 class MydlpHandler:
 	def __init__(self):
