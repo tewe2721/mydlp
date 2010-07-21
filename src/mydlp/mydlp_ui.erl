aeb5056442350c31669f135d66af364d249060d1
H. Kerem Cevahir <hkerem@gmail.com>
2010-07-21 17:20:27 +0300

    Defined UI interface.

--- src/mydlp/mydlp_ui.erl
+++ src/mydlp/mydlp_ui.erl
@@ -30,49 +30,44 @@
 -include("mydlp_ui_thrift.hrl").
 -include("mydlp_ui_types.hrl").
 
--export([start_link/0, stop/1,
-         handle_function/2
-         ]).
+-export([start_link/0,
+	stop/1,
+	handle_function/2
+	]).
+
+-export([trainClassified/3,
+	trainPublic/2,
+	removeClassified/1,
+	removeClassifiedGroup/1,
+	removePublic/1,
+	compileFilters/0
+	]).
 
 %%%%% EXTERNAL INTERFACE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-start_link() ->
-    thrift_server:start_link(9092, mydlp_ui_thrift, ?MODULE).
+start_link() -> thrift_server:start_link(9092, mydlp_ui_thrift, ?MODULE).
 
-stop(Server) ->
-    thrift_server:stop(Server),
-    ok.
+stop(Server) -> thrift_server:stop(Server), ok.
 
 %%%%% THRIFT INTERFACE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 handle_function(Function, Args) when is_atom(Function), is_tuple(Args) ->
-    case mydlp_mnesia:dyn_query(parse_func_name(Function), tuple_to_list(Args)) of
-        ok -> ok;
-        Reply -> {reply, Reply}
-    end.
+	case apply(?MODULE, Function, tuple_to_list(Args)) of
+		ok -> ok;
+		Reply -> {reply, Reply}
+	end.
 
-%%%%% HELPER FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-parse_func_name(Function) ->
-	Words = get_words(Function),
-	Atoms = words_to_atoms(Words),
-	gen_funcall(Atoms).
+%%%%% FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-get_words(Function) when is_atom(Function) -> get_words(atom_to_list(Function));
-get_words(FuncName) when is_list(FuncName) -> get_words(FuncName, [], []).
-get_words([$_|FuncName], Words, Tmp) -> get_words(FuncName, [lists:reverse(Tmp)|Words], []);
-get_words([C|FuncName], Words, Tmp) -> get_words(FuncName, Words, [C|Tmp]);
-get_words([], Words, Tmp) -> lists:reverse([lists:reverse(Tmp)|Words]).
+trainClassified(_Data, _Fileid, _Groupid) -> ok.
 
-words_to_atoms([A|Words]) -> words_to_atoms(Words, [list_to_atom(A)], []).
-words_to_atoms(["by"|Words], Items, Tmp) -> words_to_atoms(Words, 
-	[concat_words(lists:reverse(Tmp))|Items], []);
-words_to_atoms(["and"|Words], Items, Tmp) -> words_to_atoms(Words, 
-	[concat_words(lists:reverse(Tmp))|Items], []);
-words_to_atoms([W|Words], Items, Tmp) -> words_to_atoms(Words, Items, [W|Tmp]);
-words_to_atoms([], Items, Tmp) -> 
-	lists:reverse([concat_words(lists:reverse(Tmp))|Items]).
+trainPublic(_Data, _Fileid) -> ok.
 
-gen_funcall([Action, Table|Ands]) -> {Action, Table, Ands}.
+removeClassified(_Fileid) -> ok.
 
-concat_words(Words) -> list_to_atom(string:join(Words,"_")).
+removeClassifiedGroup(_Groupid) -> ok.
+
+removePublic(_Fileid) -> ok.
+
+compileFilters() -> ok.
 
