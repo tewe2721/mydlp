7f37eb308417c2cc3cb57a4944837be835509347
H. Kerem Cevahir <hkerem@gmail.com>
2011-09-12 07:20:57 -0700

    Tiny fix.

--- src/mydlp/Make.bat
+++ src/mydlp/Make.bat
@@ -1,4 +1,4 @@
-@path %path%;C:\workspace\mydlp-deployment-env\erl5.7.4\bin
+@path %path%;C:\workspace\mydlp-development-env\erl5.7.4\bin
 @set ERLC=erlc
 @set ERLCARGS=-W -D__MYDLP_ENDPOINT -D__PLATFORM_WINDOWS
 
