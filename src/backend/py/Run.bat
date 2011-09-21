0cf932b7e3f0517faefaaa8111a38411350cbc8a
Ozgur Batur <ozgurbtr@gmail.com>
2011-09-21 12:18:14 +0300

    EP win engine start scripts improvements for testing

--- src/backend/py/Run.bat
+++ src/backend/py/Run.bat
@@ -1,6 +1,3 @@
-REM @path %path%;C:\workspace\mydlp-deployment-env\Python26;C:\Program Files\MyDLP\Python26
-
-
-@set PYTHONPATH=.
-
-@python MyDLPBackendServer.py mydlp-backend-py.pid
+echo start python
+set
+python MyDLPBackendServer.py mydlp-backend-py.pid
