306e2ad06bc2476584f11c319efdd984548450ce
Ozgur Batur <ozgurbtr@gmail.com>
2011-09-22 11:15:11 +0300

    Added APPDIR enovironement variable to Run scripts.

--- src/mydlp/ManualRun.bat
+++ src/mydlp/ManualRun.bat
@@ -1,5 +1,6 @@
 set MYDLP_CONF=C:/workspace/mydlp-endpoint-win/EndPoint/Engine/mydlp/src/mydlp/mydlp-ep.conf
 set path=%path%;C:\workspace\mydlp-deployment-env\erl5.7.4\bin;C:\workspace\mydlp-deployment-env\erl5.7.4\erts-5.7.4\bin
 set MYDLPBEAMDIR="C:\workspace\mydlp-endpoint-win\EndPoint\Engine\mydlp\src\mydlp"
+set MYDLP_APPDIR="C:/Program Files/MyDLP"
 
 Run.bat
--- src/mydlp/Run.bat
+++ src/mydlp/Run.bat
@@ -2,6 +2,6 @@ echo start erlang
 set
 
 @erlsrv remove "MyDLP"
-@erlsrv add "MyDLP" -internalservicename mydlp-engine -comment "MyDLP Engine" -workdir %MYDLPBEAMDIR% -env MYDLP_CONF=%MYDLP_CONF% -sname system -args "-boot mydlp"
+@erlsrv add "MyDLP" -internalservicename mydlp-engine -comment "MyDLP Engine" -workdir %MYDLPBEAMDIR% -env MYDLP_CONF=%MYDLP_CONF% -env MYDLP_APPDIR=%MYDLP_APPDIR% -sname system -args "-boot mydlp"
 @erlsrv start "MyDLP"
 
