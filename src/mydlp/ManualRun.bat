92787185caf36c550cd88ac5fa1199f062a732a1
Ozgur Batur <ozgurbtr@gmail.com>
2011-09-22 11:21:15 +0300

    Tiny fix.

--- src/mydlp/ManualRun.bat
+++ src/mydlp/ManualRun.bat
@@ -1,6 +1,6 @@
 set MYDLP_CONF=C:/workspace/mydlp-endpoint-win/EndPoint/Engine/mydlp/src/mydlp/mydlp-ep.conf
 set path=%path%;C:\workspace\mydlp-deployment-env\erl5.7.4\bin;C:\workspace\mydlp-deployment-env\erl5.7.4\erts-5.7.4\bin
 set MYDLPBEAMDIR="C:\workspace\mydlp-endpoint-win\EndPoint\Engine\mydlp\src\mydlp"
-set MYDLP_APPDIR="C:/Program Files/MyDLP"
+set MYDLP_APPDIR="C:/workspace/mydlp-development-env"
 
 Run.bat
