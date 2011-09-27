72ef7789cf49b4eb04191288a3fddc7a6d1f8440
H. Kerem Cevahir <hkerem@gmail.com>
2011-09-27 15:31:21 -0700

    Fix.

--- src/mydlp/mydlp.hrl
+++ src/mydlp/mydlp.hrl
@@ -32,7 +32,8 @@
 	mydlp_api:acl_msg(Proto, RuleId, Action, Ip, User, To, Matcher, File, Misc)).
 
 -define(ERROR_LOG(Format, Args),
-	mydlp_logger:notify(error, "~P:~P " ++ Format, [?MODULE_STRING, ?LINE] ++ Args ++ [32])).
+	mydlp_logger:notify(error, "~P:~P " ++ Format, lists:append([ [I,32] || I <- ([?MODULE_STRING, ?LINE] ++ Args)]))
+	).
 
 -define(DEBUG(Format, Args),
 	mydlp_logger:debug_msg(?MODULE,?LINE,Format, Args)).
