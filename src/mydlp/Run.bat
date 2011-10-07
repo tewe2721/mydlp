9aa4e95a6be15e474f57b6c58e71ce2d50fc6e83
Ozgur Batur <ozgurbtr@gmail.com>
2011-10-07 17:55:47 +0300

    changed engine service name

--- src/mydlp/Run.bat
+++ src/mydlp/Run.bat
@@ -1,7 +1,7 @@
 echo start erlang
 set
 
-@erlsrv remove "MyDLP"
-@erlsrv add "MyDLP" -internalservicename mydlp-engine -comment "MyDLP Engine" -workdir %MYDLPBEAMDIR% -env MYDLP_CONF=%MYDLP_CONF% -env MYDLP_APPDIR=%MYDLP_APPDIR% -sname system -args "-boot mydlp"
-@erlsrv start "MyDLP"
+@erlsrv remove "MyDLP Engine"
+@erlsrv add "MyDLP Engine" -internalservicename mydlpengine -comment "MyDLP Engine" -workdir %MYDLPBEAMDIR% -env MYDLP_CONF=%MYDLP_CONF% -env MYDLP_APPDIR=%MYDLP_APPDIR% -sname system -args "-boot mydlp"
+@erlsrv start "MyDLP Engine"
 
