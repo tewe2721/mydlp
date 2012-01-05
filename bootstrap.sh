d3722ddd4a5db09c257b02e912e4ef20f8c0869a
H. Kerem Cevahir <hkerem@gmail.com>
2012-01-05 06:29:32 -0800

    Implemented to auto tagging to build system.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+REVISION=1
+
+if [ -n "$1" ]; then
+	REVISION=$1
+fi
+
+VERSION=$(git describe |sed -s 's/-.*$//')
+
+function applyTemplate {
+	local TMPL=$1
+	local OUT=$2
+	local TMPFILE="$OUT""~"
+
+	cp -af $TMPL $TMPFILE
+	sed -i -e "s/%%%VERSION%%%/$VERSION/g" $TMPFILE
+	sed -i -e "s/%%%REVISION%%%/$REVISION/g" $TMPFILE
+	mv -f $TMPFILE $OUT
+}
+
+echo "Current version is $VERSION-$REVISION"
+
+applyTemplate configure.ac.tmpl configure.ac
+applyTemplate debian/changelog.tmpl debian/changelog
+
 git submodule init
 git submodule update
 
---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CURRENTVERSION=$(git describe |sed -s 's/-.*$//')
+NEXTVERSION=""
+
+function incrementVersion {
+	local PRE=$(echo $CURRENTVERSION|sed -e 's/\.[0-9]$//')
+	local LAST=$(echo $CURRENTVERSION|sed -e 's/^v[0-9]\.[0-9]\.//')
+	NEXTVERSION="$PRE"".""$(( $LAST + 1 ))"
+}
+
+if [ -n "$1" -a "$1" == "incrementVersion" ]; then
+	git tag -a $NEXTVERSION -m "Tag for release $CURRENTVERSION"
+	git push --tags
+fi
+
+dpkg-buildpackage
+
