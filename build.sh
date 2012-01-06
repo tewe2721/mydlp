125c721fbae93e95e0c1a93836997b4c33c75c69
H. Kerem Cevahir <hkerem@gmail.com>
2012-01-06 01:21:21 -0800

    Tiny fix.

--- build.sh
+++ build.sh
@@ -15,8 +15,12 @@ dpkg-buildpackage || exit 1
 
 if [ -n "$1" -a "$1" == "updateVC" ]; then
 	incrementVersion
-	git tag -a $NEXTVERSION -m "Tag for release $NEXTVERSION"
-	git push --tags git@github.com:mydlp/mydlp.git
+	if [ (git tag|grep -q $NEXTVERSION) ]; then
+		echo "This revision is tagged already."
+	else
+		git tag -a $NEXTVERSION -m "Tag for release $NEXTVERSION" || exit 1
+		git push --tags git@github.com:mydlp/mydlp.git || exit 1
+	fi
 fi
 
 true
