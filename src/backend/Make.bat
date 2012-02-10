c1636e550866722f679176b67a65a5acb3339235
H. Kerem Cevahir <hkerem@gmail.com>
2012-02-10 14:56:00 +0200

    Tiny fix for hudson

--- src/backend/Make.bat
+++ src/backend/Make.bat
@@ -1,5 +1,5 @@
-@set path=%path%;C:\workspace\mydlp-development-env\apache-maven-3.0.4\bin;C:\workspace\mydlp-development-env\jdk1.7.0_02\bin
-@set JAVA_HOME=C:\workspace\mydlp-development-env\jdk1.7.0_02
+@set path=%path%;..\..\..\..\..\..\mydlp-development-env\apache-maven-3.0.4\bin;..\..\..\..\..\..\mydlp-development-env\jdk1.7.0_02\bin
+@set JAVA_HOME=..\..\..\..\..\..\mydlp-development-env\jdk1.7.0_02
 
 @mvn clean compile assembly:single
 
