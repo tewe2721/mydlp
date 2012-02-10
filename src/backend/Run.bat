0be6360cb5246425a2e0373bab81717842f9cb84
H. Kerem Cevahir <hkerem@gmail.com>
2012-02-10 12:57:41 +0200

    Added pidfile. Improvements on build scripts.

--- src/backend/Run.bat
+++ src/backend/Run.bat
@@ -1,1 +1,1 @@
-@%JRE_BIN_DIR%\java.exe -jar %BACKEND_DIR%\mydlp-backend-1.0-jar-with-dependencies.jar
+@%JRE_BIN_DIR%\java.exe -Dmydlp.appdir=%MYDLP_APPDIR% -jar %BACKEND_DIR%\mydlp-backend-1.0-jar-with-dependencies.jar
