615900022e352ca01db62ad6b6d24426f8dc97a5
hkerem <hkerem@gmail.com>
2012-08-07 12:50:52 +0300

    Fixed on windows build and run

--- src/backend/Make.bat
+++ src/backend/Make.bat
@@ -1,5 +1,10 @@
 @set path=%path%;..\..\..\..\..\..\mydlp-development-env\apache-maven-3.0.4\bin;..\..\..\..\..\..\mydlp-development-env\jdk1.7.0_02\bin
 @set JAVA_HOME=..\..\..\..\..\..\mydlp-development-env\jdk1.7.0_02
 
-@mvn clean compile assembly:single
+mvn clean
+
+cmd /c "cd ..\lib\tika-xps && mvn install && cd ..\..\backend\target && del /q tika-xps.jar && copy ..\lib\tika-xps\target\tika-xps-1.0.0.jar tika-xps.jar"
+
+cmd /c "mvn compile assembly:single && cd target && del /q mydlp-backend.jar && copy mydlp-backend-1.0-jar-with-dependencies.jar mydlp-backend.jar"
+
 
