82ba3d4671f9882be2aba6b62befe2ba20f42d41
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-10-02 09:39:10 +0300

    Implemented seclore callbacks.

--- src/backend/src/main/java/com/mydlp/backend/thrift/MydlpImpl.java
+++ src/backend/src/main/java/com/mydlp/backend/thrift/MydlpImpl.java
@@ -14,6 +14,11 @@ import org.apache.tika.metadata.Metadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mydlp.seclore.FileSecureConfigBuilder;
+import com.mydlp.seclore.FileSecureException;
+import com.mydlp.seclore.FileSecureProtect;
+import com.mydlp.seclore.MyDLPCertificateException;
+
 public class MydlpImpl implements Mydlp.Iface {
 	
 	private static Logger logger = LoggerFactory.getLogger(MydlpImpl.class);
@@ -26,6 +31,10 @@ public class MydlpImpl implements Mydlp.Iface {
 	protected static final String MIME_NOT_FOUND = "mydlp-internal/not-found";
 	
 	protected Tika tika = new Tika();
+	
+	protected FileSecureConfigBuilder secloreConfig = null;
+	
+	protected FileSecureProtect secloreProtect = null;
 		
 	protected InputStream getInputStream(final ByteBuffer buf) {
 		return new InputStream() {
@@ -105,6 +114,74 @@ public class MydlpImpl implements Mydlp.Iface {
 			}
 		}
 
+	}
+
+	@Override
+	public String secloreInitialize(String SecloreAppPath,
+			String SecloreAddress, int SeclorePort, String SecloreAppName,
+			int SecloreHotFolderCabinetId,
+			String SecloreHotFolderCabinetPassphrase) throws TException {
+		try {
+			secloreConfig = new FileSecureConfigBuilder(SecloreAppPath, SecloreAddress, SeclorePort, 
+							SecloreAppName, SecloreHotFolderCabinetId, SecloreHotFolderCabinetPassphrase);
+			try {
+				secloreConfig.installCertificateIfNotExists();
+			} catch (MyDLPCertificateException e) {
+				logger.error("An error occurred when install seclore remote certificate", e);
+			}
+			
+			secloreProtect = new FileSecureProtect(secloreConfig);
+			
+			try {
+				secloreProtect.initialize();
+				return "ok";
+			} catch (FileSecureException e) {
+				logger.error("An error occurred when initializing seclore configuration", e);
+				return e.getMessage();
+			}
+		} catch (Throwable e) {
+			logger.error("An error unexpected occurred when initializing seclore configuration", e);
+			return "mydlp.backend.seclore.initialize.unexpectedException";	
+		}
+	}
+
+	@Override
+	public String secloreProtect(String FilePath, int HotFolderId,
+			String ActivityComments) throws TException {
+		if (secloreProtect == null) {
+			return "mydlp.backend.seclore.protect.notInitialized";
+		}
+		try {
+			String fileId = secloreProtect.protect(FilePath, HotFolderId, ActivityComments);
+			return "ok " + fileId; 
+		} catch (FileSecureException e) {
+			logger.error("An error occurred when protecting document ( " +
+					FilePath + " ) woth hot folder id (" + HotFolderId + " )", e);
+			return e.getMessage();
+		} catch (Throwable e) {
+			logger.error("An error unexpected occurred when protecting document ( " +
+					FilePath + " ) woth hot folder id (" + HotFolderId + " )", e);
+			return "mydlp.backend.seclore.protect.unexpectedException";	
+		}
+	}
+
+	@Override
+	public String secloreTerminate() throws TException {
+		if (secloreProtect == null) {
+			return "mydlp.backend.seclore.terminate.notInitialized";
+		}
+		try {
+			secloreProtect.terminate();
+			secloreConfig = null;
+			secloreProtect = null;
+			return "ok"; 
+		} catch (FileSecureException e) {
+			logger.error("An error occurred when terminating seclore connection", e);
+			return e.getMessage();
+		} catch (Throwable e) {
+			logger.error("An error unexpected occurred when terminating seclore connection", e);
+			return "mydlp.backend.seclore.protect.unexpectedException";	
+		}
 	}
 
 }
