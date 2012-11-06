f2aff2ecd0c5a6a91cd402cd87fbdede5060cccf
H. Kerem Cevahir <hkerem@gmail.com>
2012-11-06 18:47:43 +0200

    Path fixes

--- src/mydlp/InteractiveRun.bat
+++ src/mydlp/InteractiveRun.bat
@@ -3,7 +3,7 @@ set MYDLP_CONF=C:/workspace/mydlp-endpoint-win/EndPoint/Engine/mydlp/src/mydlp/m
 set MYDLPBEAMDIR=C:/workspace/mydlp-endpoint-win/EndPoint/Engine/mydlp/src/mydlp
 set MYDLP_APPDIR=C:/workspace/mydlp-development-env
 set USERPROFILE=C:\workspace\mydlp-development-env
-set ERLANG_HOME=C:\workspace\mydlp-deployment-env\erl5.8.5
+set ERLANG_HOME=C:\workspace\mydlp-development-env\erl5.8.5
 
 set ERLBIN=werl.exe
 Run.bat
