f9566256bd7a5d32ea4365f9fea81f98badb7299
H. Kerem Cevahir <hkerem@gmail.com>
2012-11-06 16:50:01 +0200

    Implemented new services

--- src/mydlp/InteractiveRun.bat
+++ src/mydlp/InteractiveRun.bat
@@ -1,7 +1,8 @@
 set MYDLP_CONF=C:/workspace/mydlp-endpoint-win/EndPoint/Engine/mydlp/src/mydlp/mydlp-ep.conf
-set path=%path%;C:\workspace\mydlp-development-env\erl5.8.5\bin;C:\workspace\mydlp-development-env\erl5.8.5\erts-5.8.5\bin
+set MYDLPBEAMDIR=C:/workspace/mydlp-endpoint-win/EndPoint/Engine/mydlp/src/mydlp
 set MYDLP_APPDIR=C:/workspace/mydlp-development-env
 set USERPROFILE=C:\workspace\mydlp-development-env
+set ERLANG_HOME=C:\workspace\mydlp-deployment-env\erl5.8.5
 
-werl +fnu -setcookie pldym -sname system -boot mydlp
-
+set ERLBIN=werl.exe
+Run.bat
--- src/mydlp/Run.bat
+++ src/mydlp/Run.bat
@@ -1,7 +1,32 @@
-echo start erlang
-set
+@echo off
+set ERLANG_OPTS=+A30 +P 1048576 +fnu
 
-@erlsrv remove "MyDLP Engine"
-@erlsrv add "MyDLP Engine" -internalservicename mydlpengine -comment "MyDLP Engine" -workdir %MYDLPBEAMDIR% -env MYDLP_CONF=%MYDLP_CONF% -env MYDLP_APPDIR=%MYDLP_APPDIR% -sname system -args "+fnu -boot mydlp"
-@erlsrv start "MyDLP Engine"
+if "%ERLBIN%"=="" (
+    set ERLBIN=erl.exe
+    set ERLANG_OPTS=%ERLANG_OPTS% -noinput -noshell -detached
+)
+
+if not exist "%ERLANG_HOME%\bin\%ERLBIN%" (
+    echo.
+    echo ******************************
+    echo ERLANG_HOME not set correctly.
+    echo ******************************
+    echo.
+    exit /B
+)
+
+"%ERLANG_HOME%\bin\%ERLBIN%" ^
+%ERLANG_OPTS% ^
+-sname system ^
+
+-workdir %MYDLPBEAMDIR% ^
+-env MYDLP_CONF %MYDLP_CONF% ^
+-env MYDLP_APPDIR %MYDLP_APPDIR% ^
+-boot mydlp
+
+rem @erlsrv remove "MyDLP Engine"
+rem @erlsrv add "MyDLP Engine" -internalservicename mydlpengine -comment "MyDLP Engine" -workdir %MYDLPBEAMDIR% -sname system -args "+fnu -boot mydlp"
+rem @erlsrv start "MyDLP Engine"
+
+
 
