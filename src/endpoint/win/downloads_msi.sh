b3ed74306cccb41d1b30e9f0c23bb7514b5fa248
hkerem <hkerem@gmail.com>
2012-11-13 13:42:08 +0200

    Now getting latest in bs

--- src/endpoint/win/downloads_msi.sh
+++ src/endpoint/win/downloads_msi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-EPVER=(cat latest.text)
+[ -f "latest.txt" ] || exit 1
+EPVER=$(cat latest.txt)
 MSIFN=mydlp_$(echo $EPVER|sed -s 's/\./_/g').msi
 echo "Generating latest.msi for $EPVER"
 [ -f "latest.msi" ] && mv -f latest.msi $MSIFN
