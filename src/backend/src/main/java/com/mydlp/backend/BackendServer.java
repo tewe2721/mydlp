e8fc9dee7a2f2b7b5b04535af5e76a3b6e8fbd92
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-11-27 14:29:13 +0200

    Added logback configuration.

--- src/backend/src/main/java/com/mydlp/backend/BackendServer.java
+++ src/backend/src/main/java/com/mydlp/backend/BackendServer.java
@@ -9,15 +9,18 @@ import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.server.TThreadPoolServer.Args;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mydlp.backend.thrift.Mydlp;
 import com.mydlp.backend.thrift.MydlpImpl;
 
 public class BackendServer {
 	
+	private static Logger logger = LoggerFactory.getLogger(BackendServer.class);
+	
 	protected TServerSocket serverTransport = null;
 	
-
 	@SuppressWarnings("unchecked")
 	public void listen() {
 		try {
@@ -28,7 +31,7 @@ public class BackendServer {
 			Factory protFactory = new TBinaryProtocol.Factory(true, true);
 			TServer server = new TThreadPoolServer(new Args(serverTransport)
 					.processor(processor).protocolFactory(protFactory));
-			System.out.println("Starting server on port 9090 ...");
+			logger.debug("Starting server on port 9090.");
 			server.serve();
 		} catch (TTransportException e) {
 			e.printStackTrace();
