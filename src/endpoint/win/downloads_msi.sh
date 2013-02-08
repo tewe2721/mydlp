ed75e2d84e21f1a6e13d21ac21ea79f5b6a5898c
hkerem <hkerem@gmail.com>
2013-02-08 14:50:25 +0200

    Refinements in msi inclusion

--- src/endpoint/win/downloads_msi.sh
+++ src/endpoint/win/downloads_msi.sh
@@ -3,7 +3,5 @@
 EPVER=$(cat latest.txt)
 MSIFN=mydlp_$(echo $EPVER|sed -s 's/\./_/g').msi
 echo "Generating latest.msi for $EPVER"
-[ -f "latest.msi" ] && mv -f latest.msi $MSIFN
-wget --quiet -Nc http://ftp/ftp/pub/mydlp/endpoint/$MSIFN || exit 1
-mv -f $MSIFN latest.msi || exit 1
-
+mkdir -p msi; cd msi; wget --quiet -Nc http://ftp/ftp/pub/mydlp/endpoint/$MSIFN || exit 1
+[ -f "$MSIFN" ] || exit 1
