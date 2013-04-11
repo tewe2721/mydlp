e454508b1bb91c6009beac88f623794e1df06ae1
hkerem <hkerem@gmail.com>
2013-04-11 23:46:11 +0300

    .

--- src/mydlp/mydlp_scheduler.erl
+++ src/mydlp/mydlp_scheduler.erl
@@ -50,13 +50,12 @@
 
 -record(state, {
 	item_queue,
-	queue_size = 0,
-	has_failure = false
+	consume_inprog=false
 }).
 
 %%%%%%%%%%%%%  API
 
-s(Item) -> gen_server:cast(?MODULE, {p, Item}).
+s(Item) -> gen_server:cast(?MODULE, {s, Item}).
 
 %%%%%%%%%%%%%% gen_server handles
 
@@ -66,31 +65,28 @@ handle_call(stop, _From, State) ->
 handle_call(_Msg, _From, State) ->
 	{noreply, State}.
 
-handle_cast({p, Ref, Item}, #state{item_queue=Q, queue_size=QS, has_failure=HF} = State) ->
-	Q1 = queue:in({Ref, Item}, Q),
-	ItemSize = predict_serialized_size(Item),
-	NextQS = QS+ItemSize,
-	case NextQS > ?CFG(maximum_push_size) of
-		true -> consume_item();
-		false -> ok end,
-	case HF of
-		false -> mydlp_spool:consume_next("log");
-		true -> ok end,
-	{noreply,State#state{item_queue=Q1, queue_size=NextQS}};
+handle_cast({s, Item}, #state{item_queue=Q, consume_inprog=InProg} = State) ->
+	Q1 = queue:in(Item, Q),
+	case InProg of
+		true -> ok;
+		false -> consume_item() end,
+	{noreply,State#state{item_queue=Q1, consume_inprog=true}};
 
 handle_cast(consume_item, #state{item_queue=Q} = State) ->
-	case queue:is_empty(Q) of
-		false -> HF = try RefItemList = queue:to_list(Q),
-				process(RefItemList),
-				false
-			catch Class:Error ->
-				?ERROR_LOG("Push Item Consume: Error occured: Class: ["?S"]. Error: ["?S"].~nStack trace: "?S"~n.~nState: "?S"~n ",
-					[Class, Error, erlang:get_stacktrace(), State]), true end,
-			consume_item(?CFG(sync_interval)),
-			{noreply, State#state{item_queue=queue:new(), queue_size=0, has_failure=HF}};
-		true -> mydlp_spool:consume_next("log"),
-			consume_item(?CFG(sync_interval)),
-			{noreply, State} end;
+	case queue:out(Q) of
+                {{value, Item}, Q1} ->
+                        try     process(Item)
+                        catch Class:Error ->
+                                ?ERROR_LOG("Scheduler Queue Consume: Error occured: "
+                                                "Class: ["?S"]. Error: ["?S"].~n"
+                                                "Stack trace: "?S"~n.Item: "?S"~nState: "?S"~n ",
+                                                [Class, Error, erlang:get_stacktrace(), Item, State]) end,
+                                %%% TODO: for some errors we many return original queue
+                        consume(),
+                        {noreply, State#state{item_queue=Q1}};
+                {empty, _} ->
+                        {noreply, State#state{consume_inprog=false}}
+        end;
 
 handle_cast(_Msg, State) ->
 	{noreply, State}.
@@ -133,100 +129,9 @@ code_change(_OldVsn, State, _Extra) ->
 
 %%%%%%%%%%%%%%%%% internal
 
-process_ril(RefItemList) ->
-	RefList = [ R || {R,_I} <- RefItemList],
-	ItemList = [ I || {_R,I} <- RefItemList],
-	try	ok = process_item(ItemList),
-		lists:foreach(fun(R) -> mydlp_spool:delete(R) end, RefList)
-	catch Class:Error ->
-		?ERROR_LOG("Process Item : Error occured: Class: ["?S"]. Error: ["?S"].~nStack trace: "?S"~n.~nRefItemList: "?S"~n ",
-			[Class, Error, erlang:get_stacktrace(), RefItemList])
-	after 	lists:foreach(fun(R) -> mydlp_spool:release(R) end, RefList) end.
-
-process_item(Item)  ->  
-	ItemBin = erlang:term_to_binary(Item, [compressed]),
-	ItemId = new_item_id(),
-	ItemSize = size(ItemBin),
-	ChunkNumTotal = (ItemSize div ?CFG(maximum_push_size)) + 1,
-	process_item(ItemId, ItemBin, ItemSize, 1, ChunkNumTotal).
-
-% process_item(_Item) -> ok. % TODO log unkown item.
-
-process_item(_ItemId, _ItemBin, RemainingItemSize, _ChunkNumTotal, _ChunkNumTotal) when RemainingItemSize < 0 ->
-	throw({error, negative_remaining_item_size});
-process_item(ItemId, ItemBin, RemainingItemSize, _ChunkNum = ChunkNumTotal, ChunkNumTotal) ->
-	ChunkSize = RemainingItemSize,
-	<<ChunkData:ChunkSize/binary>> = ItemBin,
-	push_chunk(ItemId, ChunkData, ChunkNumTotal, ChunkNumTotal);
-process_item(ItemId, ItemBin, RemainingItemSize, ChunkNum, ChunkNumTotal) ->
-	ChunkSize = ?CFG(maximum_push_size),
-	<<ChunkData:ChunkSize/binary, ItemRestBin/binary >> = ItemBin,
-	push_chunk(ItemId, ChunkData, ChunkNum, ChunkNumTotal),
-	process_item(ItemId, ItemRestBin, RemainingItemSize - ChunkSize, ChunkNum + 1, ChunkNumTotal).
-
-push_chunk(ItemId, ChunkData, ChunkNum, ChunkNumTotal) ->
-	ItemIdS = integer_to_list(ItemId),
-	ChunkNumS = integer_to_list(ChunkNum),
-	ChunkNumTotalS = integer_to_list(ChunkNumTotal),
-	Url = "https://" ++ ?CFG(management_server_address) ++ "/receive?o=push&" ++
-			"i=" ++ ItemIdS ++ "&c=" ++ ChunkNumS ++ "&t=" ++ ChunkNumTotalS,
-	case http_req(Url, ChunkData) of
-		{ok, <<"error">>} -> throw(http_returned_error);
-                {ok, <<"invalid", _/binary>>} -> mydlp_api:delete_endpoint_key(), throw(http_returned_invalid);
-		{ok, <<"ok">>} -> ok;
-		Else -> throw(Else) end.
-
-new_item_id() ->
-	Url = "https://" ++ ?CFG(management_server_address) ++ "/receive?o=begin",
-	case http_req(Url) of
-		{ok, <<"error">>} -> throw(http_returned_error);
-                {ok, <<"invalid", _/binary>>} -> mydlp_api:delete_endpoint_key(), throw(http_returned_invalid);
-		{ok, <<"null">>} -> throw(http_returned_null);
-		{ok, Ret} -> mydlp_api:binary_to_integer(Ret);
-		Else -> throw(Else) end.
-
-http_req(Url) -> http_req(Url, <<>>).
-
-http_req(Url, Data) when is_binary(Data) ->
-	Payload = case mydlp_api:encrypt_payload(Data) of
-		retry -> throw({error, encrypt_payload_retry});
-		P when is_binary(P) -> P end,
-	ReqRet = (catch httpc:request(post, {Url, [], "application/octet-stream", Payload}, [], [])),
-	http_req1(ReqRet).
-
-http_req1(ReqRet) -> 
-        case ReqRet of
-                {ok, {{_HttpVer, Code, _Msg}, _Headers, Body0}} -> 
-			Body = case Body0 of
-				L when is_list(L) -> list_to_binary(L);
-				B when is_binary(B) -> B end,
-                        case {Code, Body} of
-				{200, <<"error">>} -> throw(http_returned_error);
-				{200, <<"invalid">>} -> mydlp_api:delete_endpoint_key(), throw(http_returned_invalid);
-				{200, <<"null">>} -> throw(http_returned_null);
-                                {200, RetBody} -> case mydlp_api:decrypt_payload(RetBody) of
-					retry -> throw({error, decrypt_payload_retry});
-					P when is_binary(P) -> {ok, P} end;
-                                {Else1, _Data} -> ?ERROR_LOG("ITEMPUSH: An error occured during HTTP req: Code="?S"~n", [Else1]),
-						{error, {http_code, Else1}} end;
-                Else -> ?ERROR_LOG("ITEMPUSH: An error occured during HTTP req: Obj="?S"~n", [Else]),
-				{error, {http_req_not_ok, Else}} end.
-
-predict_serialized_size({endpoint_log, #log{file= Files}}) ->
-	300 + lists:sum([predict_file_size(F)||F <- Files]);
-predict_serialized_size({endpoint_log, _LogTerm}) -> 300;
-predict_serialized_size({endpoint_opr_log, _Context, _Term}) -> 500;
-predict_serialized_size(Else) -> 
-	?ERROR_LOG("PREDICTSIZE: Unknown item. Cannot predict. Return maximum_push_size+1 as size: Item="?S"~n", [Else]),
-	?CFG(maximum_push_size) + 1.
-
-predict_file_size(#file{data=undefined}) -> 0;
-predict_file_size(#file{data=Data}) -> size(Data).
-
-
-strip_item({endpoint_log, #log{file=Files}=LogTerm}) ->
-	StrippedFiles = mydlp_api:remove_all_data(Files),
-	{endpoint_log, LogTerm#log{file=StrippedFiles}}.
+process(Item) ->
+	erlang:display(Item),
+	ok.
 
 
 -endif.
