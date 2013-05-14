8e7830d4051660162ef302671b43db6c9f14de9d
hkerem <hkerem@gmail.com>
2013-05-15 00:03:43 +0300

    Fix on queries

--- src/schema/diffs/2.0.34-1___mydlp___p001.sql
+++ src/schema/diffs/2.0.34-1___mydlp___p001.sql
@@ -1,3 +1,7 @@
-ALTER TABLE SourceDomainName DROP FOREIGN KEY FK6DBC240A1955299D;
-DROP TABLE SourceDomainName;
 ALTER TABLE RuleItem ADD COLUMN ruleColumn VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '';
+SET @@foreign_key_checks = 0;
+ALTER TABLE SourceDomainName DROP FOREIGN KEY FK6DBC240A1955299D;
+UPDATE RuleItem SET ruleColumn="SOURCE" WHERE item_id IN (SELECT id FROM SourceDomainName);
+INSERT INTO Domain (id, destinationString) VALUES (SELECT id, sourceDomain as destinationString FROM SourceDomainName);
+DROP TABLE SourceDomainName;
+SET @@foreign_key_checks = 1;
