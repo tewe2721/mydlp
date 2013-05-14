d29a9e5bfe2258259e06fa4d9d066567067e5ee5
hkerem <hkerem@gmail.com>
2013-05-15 00:29:39 +0300

    Tiny fix

--- src/schema/diffs/2.0.34-1___mydlp___p001.sql
+++ src/schema/diffs/2.0.34-1___mydlp___p001.sql
@@ -2,6 +2,6 @@ ALTER TABLE RuleItem ADD COLUMN ruleColumn VARCHAR(255) CHARACTER SET utf8mb4 CO
 SET @@foreign_key_checks = 0;
 ALTER TABLE SourceDomainName DROP FOREIGN KEY FK6DBC240A1955299D;
 UPDATE RuleItem SET ruleColumn="SOURCE" WHERE item_id IN (SELECT id FROM SourceDomainName);
-INSERT INTO Domain (id, destinationString) VALUES (SELECT id, sourceDomain as destinationString FROM SourceDomainName);
+INSERT INTO Domain (id, destinationString) SELECT id, sourceDomain as destinationString FROM SourceDomainName;
 DROP TABLE SourceDomainName;
 SET @@foreign_key_checks = 1;
