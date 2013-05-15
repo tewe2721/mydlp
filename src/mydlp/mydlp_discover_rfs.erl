736068cb47ec0af010e91c8b6ec61e451e511b54
hkerem <hkerem@gmail.com>
2013-05-15 16:07:47 +0300

    releasing mounts on init

--- src/mydlp/mydlp_discover_rfs.erl
+++ src/mydlp/mydlp_discover_rfs.erl
@@ -187,7 +187,7 @@ stop() ->
 init([]) ->
 	%timer:send_after(60000, startup),
 	filelib:ensure_dir(?MOUNT_PATH),
-	finished(),
+	finished(), % to trigger releasem mounts
 	{ok, #state{mount_dict=dict:new()}}.
 
 terminate(_Reason, _State) ->
