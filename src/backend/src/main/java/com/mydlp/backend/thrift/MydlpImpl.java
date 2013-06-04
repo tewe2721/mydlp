64a448098ccda6e8b270d341ad9043cfc97c1149
Hseyin Kerem Cevahir <hkerem@gmail.com>
2013-06-05 02:34:39 +0300

    Reverted

--- src/backend/src/main/java/com/mydlp/backend/thrift/MydlpImpl.java
+++ src/backend/src/main/java/com/mydlp/backend/thrift/MydlpImpl.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
-import java.io.StringReader;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
@@ -27,7 +26,7 @@ import com.mydlp.seclore.FileSecureProtect;
 import com.mydlp.seclore.MyDLPCertificateException;
 
 public class MydlpImpl implements Mydlp.Iface {
-
+	
 	private static Logger logger = LoggerFactory.getLogger(MydlpImpl.class);
 
 	protected static final String DEFAULT_ENCODING = "UTF-8";
@@ -36,26 +35,26 @@ public class MydlpImpl implements Mydlp.Iface {
 	protected static final ByteBuffer ERROR = Charset.forName(DEFAULT_ENCODING)
 			.encode(CharBuffer.wrap("mydlp-internal/error"));
 	protected static final String MIME_NOT_FOUND = "mydlp-internal/not-found";
-
+	
 	protected Tika tika = null;
-
+	
 	protected HtmlCleaner cleaner = null;
-
+	
 	protected FileSecureConfigBuilder secloreConfig = null;
-
+	
 	protected FileSecureProtect secloreProtect = null;
-
+	
 	protected Tika getTika() {
 		if (tika == null) {
 			tika = new Tika();
 		}
 		return tika;
 	}
-
+	
 	protected HtmlCleaner getCleaner() {
 		if (cleaner == null) {
 			cleaner = new HtmlCleaner();
-			CleanerProperties props = cleaner.getProperties();
+		    CleanerProperties props = cleaner.getProperties();
 			props.setAllowHtmlInsideAttributes(true);
 			props.setAllowMultiWordAttributes(true);
 			props.setRecognizeUnicodeChars(true);
@@ -63,7 +62,7 @@ public class MydlpImpl implements Mydlp.Iface {
 		}
 		return cleaner;
 	}
-
+	
 	protected InputStream getInputStream(final ByteBuffer buf) {
 		return new InputStream() {
 			public synchronized int read() throws IOException {
@@ -101,40 +100,15 @@ public class MydlpImpl implements Mydlp.Iface {
 			}
 		}
 	}
-
+	
 	protected Boolean isMemoryError(Throwable t) {
 		if (t instanceof OutOfMemoryError)
 			return true;
-
+		
 		Throwable cause = t.getCause();
 		if (cause == null)
 			return false;
 		return isMemoryError(cause);
-	}
-
-	protected Reader getMeta(Metadata metadata) {
-		StringBuffer buffer = new StringBuffer();
-		for (int i = 0; i < metadata.names().length; i++) {
-			String name = metadata.names()[i];
-			if (name.equals(Metadata.CONTENT_TYPE))
-				continue;
-			if (name.equals(Metadata.CONTENT_TYPE))
-				continue;
-			buffer.append(name + " : " + metadata.get(name) + "\n");
-		}
-		return new StringReader(buffer.toString());
-	}
-	
-	protected ByteBuffer getFinalBuffer(Reader metaReader, Reader textReader) 
-			throws IOException {
-		byte[] metaBytes = IOUtils.toByteArray(metaReader, DEFAULT_ENCODING);
-		byte[] textBytes = IOUtils.toByteArray(textReader, DEFAULT_ENCODING);
-		byte[] finalBytes = new byte[metaBytes.length + textBytes.length];
-
-		System.arraycopy(metaBytes,	0,	finalBytes,	0,					metaBytes.length);
-		System.arraycopy(textBytes,	0,	finalBytes,	metaBytes.length,	textBytes.length);
-		
-		return ByteBuffer.wrap(finalBytes);
 	}
 
 	@Override
@@ -146,14 +120,16 @@ public class MydlpImpl implements Mydlp.Iface {
 			if (FileName != null && FileName.length() > 0)
 				metadata.add(Metadata.RESOURCE_NAME_KEY, FileName);
 			metadata.add(Metadata.CONTENT_TYPE, MimeType);
-			Reader textReader = getTika().parse(inputStream, metadata);
-			Reader metaReader = getMeta(metadata);
-			return getFinalBuffer(metaReader, textReader);
+			Reader reader = getTika().parse(inputStream, metadata);
+			return ByteBuffer.wrap(IOUtils.toByteArray(reader, DEFAULT_ENCODING));
 		} catch (Throwable e) {
-			if (isMemoryError(e)) {
+			if (isMemoryError(e))
+			{
 				logger.error("Can not allocate required memory", e);
 				return EMPTY;
-			} else {
+			}
+			else
+			{
 				logger.error("Can not read text", e);
 				return ERROR;
 			}
@@ -165,7 +141,7 @@ public class MydlpImpl implements Mydlp.Iface {
 			}
 		}
 	}
-
+	
 	@Override
 	public ByteBuffer getUnicodeText(String Encoding, ByteBuffer Data)
 			throws TException {
@@ -174,18 +150,18 @@ public class MydlpImpl implements Mydlp.Iface {
 			Metadata metadata = new Metadata();
 			metadata.add(Metadata.RESOURCE_NAME_KEY, "sample.txt");
 			metadata.add(Metadata.CONTENT_TYPE, "text/plain");
-			if (Encoding != null && Encoding.length() > 0
-					&& !Encoding.equals("unknown"))
-				;
-			metadata.add(Metadata.CONTENT_ENCODING, Encoding);
+			if (Encoding != null && Encoding.length() > 0 && !Encoding.equals("unknown"));
+				metadata.add(Metadata.CONTENT_ENCODING, Encoding);
 			Reader reader = getTika().parse(inputStream, metadata);
-			return ByteBuffer.wrap(IOUtils
-					.toByteArray(reader, DEFAULT_ENCODING));
+			return ByteBuffer.wrap(IOUtils.toByteArray(reader, DEFAULT_ENCODING));
 		} catch (Throwable e) {
-			if (isMemoryError(e)) {
+			if (isMemoryError(e))
+			{
 				logger.error("Can not allocate required memory", e);
 				return EMPTY;
-			} else {
+			}
+			else
+			{
 				logger.error("Can not read unicode text", e);
 				return ERROR;
 			}
@@ -197,22 +173,21 @@ public class MydlpImpl implements Mydlp.Iface {
 			}
 		}
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<String> extractLinks(ByteBuffer HTMLData) throws TException {
 		List<String> links = new ArrayList<String>();
 		try {
 			TagNode rootNode = getCleaner().clean(getInputStream(HTMLData));
-			for (TagNode aTag : (List<TagNode>) rootNode.getElementListByName(
-					"a", true)) {
+			for (TagNode aTag : (List<TagNode>) rootNode.getElementListByName ("a", true))
+			{
 				try {
-					String link = aTag.getAttributeByName("href");
-					if (link != null && link.length() > 0)
-						links.add(link);
+				    String link = aTag.getAttributeByName ("href");
+				    if (link != null && link.length () > 0) links.add (link);
 				} catch (Throwable t) {
 					logger.error("Can not extract link", t);
-				}
+				} 
 			}
 		} catch (IOException e) {
 			logger.error("Can not parse HTML", e);
@@ -224,40 +199,32 @@ public class MydlpImpl implements Mydlp.Iface {
 	public String secloreInitialize(String SecloreAppPath,
 			String SecloreAddress, int SeclorePort, String SecloreAppName,
 			int SecloreHotFolderCabinetId,
-			String SecloreHotFolderCabinetPassphrase, int SeclorePoolSize)
-			throws TException {
+			String SecloreHotFolderCabinetPassphrase,
+			int SeclorePoolSize) throws TException {
 		try {
 			if (secloreProtect != null || secloreConfig != null)
 				secloreTerminate();
-
-			secloreConfig = new FileSecureConfigBuilder(SecloreAppPath,
-					SecloreAddress, SeclorePort, SecloreAppName,
-					SecloreHotFolderCabinetId,
-					SecloreHotFolderCabinetPassphrase, SeclorePoolSize);
+			
+			secloreConfig = new FileSecureConfigBuilder(SecloreAppPath, SecloreAddress, SeclorePort, 
+							SecloreAppName, SecloreHotFolderCabinetId, SecloreHotFolderCabinetPassphrase, SeclorePoolSize);
 			try {
 				secloreConfig.installCertificateIfNotExists();
 			} catch (MyDLPCertificateException e) {
-				logger.error(
-						"An error occurred when install seclore remote certificate",
-						e);
+				logger.error("An error occurred when install seclore remote certificate", e);
 			}
-
+			
 			secloreProtect = new FileSecureProtect(secloreConfig);
-
+			
 			try {
 				secloreProtect.initialize();
 				return "ok";
 			} catch (FileSecureException e) {
-				logger.error(
-						"An error occurred when initializing seclore configuration",
-						e);
+				logger.error("An error occurred when initializing seclore configuration", e);
 				return e.getMessage();
 			}
 		} catch (Throwable e) {
-			logger.error(
-					"An error unexpected occurred when initializing seclore configuration",
-					e);
-			return "mydlp.backend.seclore.initialize.unexpectedException";
+			logger.error("An error unexpected occurred when initializing seclore configuration", e);
+			return "mydlp.backend.seclore.initialize.unexpectedException";	
 		}
 	}
 
@@ -277,20 +244,16 @@ public class MydlpImpl implements Mydlp.Iface {
 				return "mydlp.backend.seclore.protect.canNotRead";
 			if (!file.canWrite())
 				return "mydlp.backend.seclore.protect.canNotWrite";
-			String fileId = secloreProtect.protect(file.getAbsolutePath(),
-					HotFolderId, ActivityComments);
-			return "ok " + fileId;
+			String fileId = secloreProtect.protect(file.getAbsolutePath(), HotFolderId, ActivityComments);
+			return "ok " + fileId; 
 		} catch (FileSecureException e) {
-			logger.error(
-					"An error occurred when protecting document ( " + FilePath
-							+ " ) woth hot folder id (" + HotFolderId + " )", e);
+			logger.error("An error occurred when protecting document ( " +
+					FilePath + " ) woth hot folder id (" + HotFolderId + " )", e);
 			return e.getMessage();
 		} catch (Throwable e) {
-			logger.error(
-					"An error unexpected occurred when protecting document ( "
-							+ FilePath + " ) woth hot folder id ("
-							+ HotFolderId + " )", e);
-			return "mydlp.backend.seclore.protect.unexpectedException";
+			logger.error("An error unexpected occurred when protecting document ( " +
+					FilePath + " ) woth hot folder id (" + HotFolderId + " )", e);
+			return "mydlp.backend.seclore.protect.unexpectedException";	
 		}
 	}
 
@@ -303,16 +266,13 @@ public class MydlpImpl implements Mydlp.Iface {
 			secloreProtect.terminate();
 			secloreConfig = null;
 			secloreProtect = null;
-			return "ok";
+			return "ok"; 
 		} catch (FileSecureException e) {
-			logger.error(
-					"An error occurred when terminating seclore connection", e);
+			logger.error("An error occurred when terminating seclore connection", e);
 			return e.getMessage();
 		} catch (Throwable e) {
-			logger.error(
-					"An error unexpected occurred when terminating seclore connection",
-					e);
-			return "mydlp.backend.seclore.protect.unexpectedException";
+			logger.error("An error unexpected occurred when terminating seclore connection", e);
+			return "mydlp.backend.seclore.protect.unexpectedException";	
 		}
 	}
 
